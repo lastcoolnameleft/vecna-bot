4212a915ccfba8ef86f67dded5caa555e77ef409
Tommy Falgout <tommy@lastcoolnameleft.com>
2017-02-17 23:18:01 -0600

    big update.  Figured out some card and debugging

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import * as builder from 'botbuilder';
 import * as argv from 'yargs';
 import * as Roll from 'roll';
+import * as config from './config';
 
 var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID ? process.env.MICROSOFT_APP_ID : argv.id,
@@ -35,41 +36,58 @@ bot.dialog('rollDialog', (session, args) => {
 
 bot.dialog('createCharacterDialog', [
   (session) => {
-    session.dialogData.character = {};
+    session.userData.character = {};
+    console.log(session.userData);
     builder.Prompts.text(session, 'What do you want to name your character?');
   },
   (session, results) => {
     console.log(results.response);
-    session.dialogData.character.name = results.response;
-    session.send('Welcome, ' + session.dialogData.character.name);
-    builder.Prompts.choice(session, "What race?", 'Human|Dwarf|Elf');
+    console.log(session.userData);
+    session.userData.character.name = results.response;
+    session.send('Welcome, ' + session.userData.character.name);
+    console.log(config);
+    builder.Prompts.choice(session, "What race?", Object.keys(config.races));
   },
   (session, results) => {
     console.log(results.response);
-    session.dialogData.character.race = results.response.entity;
-    session.send(session.dialogData.character.race + ' is a good choice.');
-    builder.Prompts.choice(session, "What class?", 'Wizard|Rogue|Warrior');
+    session.userData.character.race = results.response.entity;
+    session.send(session.userData.character.race + ' is a good choice.');
+    builder.Prompts.choice(session, "What class?", Object.keys(config.classes));
   },
   (session, results) => {
     console.log(results.response);
-    session.dialogData.character.class = results.response.entity;
-    session.send(session.dialogData.character.class + ' is a good choice.');
-    console.log(session.dialogData);
-    session.endDialogWithResult({ response: session.dialogData });
+    session.userData.character.class = results.response.entity;
+    session.endDialog(session.userData.character.class + ' is a good choice.');
+    console.log(session.userData);
   }
 ]).triggerAction({ matches: /character/i });
 
+
 bot.dialog('whoami', (session) => {
-  console.log(session.dialogData);
-  if (!session.dialogData.character) {
+  console.log(session.userData);
+  if (!session.userData.character) {
     session.endDialog('You need to create a character first.  Type: character');
   } else {
-    let response = 'name: ' + session.dialogData.character.name + "\n"
-      + 'race: ' + session.dialogData.character.race + "\n"
-      + 'class: ' + session.dialogData.character.class + "\n";
-      session.endDialogWithResult(response);
+    let response = 'name: ' + session.userData.character.name + "\n\n"
+      + 'race: ' + session.userData.character.race + "\n\n"
+      + 'class: ' + session.userData.character.class;
+      session.send(response);
+      session.endDialog(response);
   }
 }).triggerAction({ matches: /whoami/i });
+
+/*
+bot.dialog('card', (session) => {
+    let card = new builder.AnimationCard(session)
+        .title('Microsoft Bot Framework')
+        .subtitle('Animation Card')
+        .media([
+            { url: 'http://i.giphy.com/Ki55RUbOV5njy.gif' }
+        ]);
+    var msg = new builder.Message(session).addAttachment(card);
+    session.send(msg);
+}).triggerAction({ matches: /card/i });
+*/
 
 var restify = require('restify');
 var server = restify.createServer();
